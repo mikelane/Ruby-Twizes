6107b8e6bfaa1b4f16824a4304e19dde74c7a3a3
Michael Lane <mikelane@gmail.com>
2015-08-29 01:28:09 -0700

    Some code cleanup and ruby-ifying

--- twizes.rb
+++ twizes.rb
@@ -2,15 +2,15 @@ require 'tweetstream'
 require 'yaml'
 
 # Load the Twitter API oauth key information from a file. Keep the info off github!
-configInfo = Psych.load_file("config.yaml")
-configInfo = Psych.load(configInfo)
+config_info = Psych.load_file('config.yaml')
+config_info = Psych.load(config_info)
 
 # Configure the TweetStream API
 TweetStream.configure do |config|
-  config.consumer_key       = configInfo['consumer_key']
-  config.consumer_secret    = configInfo['consumer_secret']
-  config.oauth_token        = configInfo['oauth_token']
-  config.oauth_token_secret = configInfo['oauth_token_secret']
+  config.consumer_key       = config_info['consumer_key']
+  config.consumer_secret    = config_info['consumer_secret']
+  config.oauth_token        = config_info['oauth_token']
+  config.oauth_token_secret = config_info['oauth_token_secret']
   config.auth_method        = :oauth
 end
 
@@ -19,7 +19,7 @@ end
 retweets = Hash.new         # Create an empty hash
 req_len = 100               # The length the hash must be before it gets written to a file
 len_increment = 100         # The amount to increment the hash length by after dumping it to a file
-file = "retweets-log.yml"   # The name of the logging file.
+file = 'retweets-log.yml'   # The name of the logging file.
 # @todo open and parse an existing log file if one exists. Allow the hash to persist between sessions.
 # @todo initialize the req_len to the length of the existing hash if one is found.
 
@@ -31,13 +31,13 @@ TweetStream::Client.new.track('retweet', 'RT') do |status|
   original_tweet = status.retweeted_status
 
   # If we found an original tweet...
-  if !original_tweet.nil?
+  unless original_tweet.nil?
 
     # If the hash does not contain the original tweet id...
     if retweets[original_tweet.id].nil?
 
       # If the original tweet id contains the string "win "
-      if original_tweet.text.downcase.include? "win "
+      if original_tweet.text.downcase.include? 'win '
 
         # @todo change this to actually retweet. Currently, print out the tweet for testing
         puts "Retweet: #{original_tweet.text}"
@@ -45,18 +45,16 @@ TweetStream::Client.new.track('retweet', 'RT') do |status|
         # Detect a follow requirement
         # @todo change this to actually follow. Currently, print out the username to follow
         # @todo implement a queue to deal with Twitter follow limits.
-        if original_tweet.text.downcase.include? "follow"
-          puts "Follow:  [@#{original_tweet.user.screen_name}]"
-        end
+        puts "Follow:  [@#{original_tweet.user.screen_name}]" if original_tweet.text.downcase.include? "follow"
 
         # Add the tweet id to the hash, initialized to value 1
         retweets[original_tweet.id] = 1
       end
 
-    # If the has already has the tweet id...
+      # If the has already has the tweet id...
     else
       # Output message for testing.
-      puts "Already Retweeted!"
+      puts 'Already Retweeted!'
       # Increment the value. This keeps track of how many times an original has been retweeted.
       retweets[original_tweet.id] += 1
     end
@@ -64,8 +62,8 @@ TweetStream::Client.new.track('retweet', 'RT') do |status|
     # Once the hash gets a certain size, dump it to a file as YAML.
     if retweets.length > req_len
       obj = YAML::dump(retweets)
-      File.open(file, 'w') {|f| f.write obj.to_yaml}
-      req_len += len_increment    # Increment the required length
+      File.open(file, 'w') { |f| f.write obj.to_yaml }
+      req_len += len_increment # Increment the required length
     end
   end
 end
